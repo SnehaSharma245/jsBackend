00027fea995eb34fa535f142a2c5713e48a20901
SnehaSharma245 <snehav2109@gmail.com>
2024-10-06 23:15:00 +0530

    cloudinary setup

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ dotenv.config({
 import connectDB from "./db/index.js";
 connectDB()
   .then(() => {
+    app.get("/", (req, res) => {
+      res.send("Hello World");
+    });
+
     app.listen(process.env.PORT || 8000, () => {
       console.log(`Server is running at port ${process.env.PORT}`);
     });
