347dcc5b8a0e32a4eca825fe26fd725113f7ec4a
SnehaSharma245 <snehav2109@gmail.com>
2024-10-10 21:39:51 +0530

    register controller and some debugging

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -1,4 +1,5 @@
-import { multer } from "multer";
+import multer from "multer";
+
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
     cb(null, "./public/temp");
@@ -11,4 +12,5 @@ const storage = multer.diskStorage({
   },
 });
 
-export const upload = multer({ storage });
+const upload = multer({ storage: storage });
+export { upload };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,23 @@
 import { Router } from "express";
 import { registerUser } from "../controllers/user.controller.js";
+import { upload } from "../middlewares/multer.middleware.js";
 const router = Router();
-router.route("/register").post(registerUser);
+//Ye line /register endpoint ke liye route define kar rahi hai.
+//.post(...): Ye method HTTP POST request handle karne ke liye use hota hai. Iska matlab hai ki jab koi client is route par data bhejta hai, toh is route ke andar jo function diya gaya hai, wo execute hoga.
+
+router.route("/register").post(
+  upload.fields([
+    {
+      name: "avatar",
+      maxCount: 1,
+    },
+    {
+      name: "coverImage",
+      maxCount: 1,
+    },
+  ]),
+  //Yahan registerUser function ko call kiya ja raha hai, jo user registration ka logic handle karega, jab files upload ho jayengi.
+  registerUser
+);
+
 export default router;
